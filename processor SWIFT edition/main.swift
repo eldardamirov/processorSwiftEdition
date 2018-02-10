9e14b2fe1a7358c5ddb5b24a84858d24a6fda196
eldardamirov <eldardamirov75@mail.ru>
2018-02-10 18:27:09 +0300

    Initial Commit

---
+++ processor SWIFT edition/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  processor SWIFT edition
+//
+//  Created by Эльдар Дамиров on 10.02.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
